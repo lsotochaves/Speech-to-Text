faac54879649751ed9b2c7a19a95cb51b6dbc8c9
lsotochaves <luis.sotochaves@ucr.ac.cr>
2026-09-14 02:43:00 -0600

    updated requirements .txt with correct torch package

--- model_test/model.py
+++ model_test/model.py
@@ -1,6 +1,5 @@
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-from datasets import load_dataset
 
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
