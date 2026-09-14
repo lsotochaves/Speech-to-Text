0a3d1e340c357f9859246a3cc936397ad2acb4da
lsotochaves <luis.sotochaves@ucr.ac.cr>
2026-09-14 02:34:40 -0600

    installed library transformers

---
+++ model_test/model.py
@@ -0,0 +1,7 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from datasets import load_dataset
+
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+print(device)
