a5298ece7619134d5c448f1544ceec3640a6ef1e
lsotochaves <luis.sotochaves@ucr.ac.cr>
2026-09-14 18:47:51 -0600

    First model test ran on GPU

--- model_test/model.py
+++ model_test/model.py
@@ -1,6 +1,31 @@
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from datasets import load_dataset
 
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-print(device)
+dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3-turbo"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, dtype=dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    dtype=dtype,
+    device=device,
+)
+
+dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
+sample = dataset[0]["audio"]
+
+result = pipe(sample, return_timestamps=True)
+print(result["text"])
